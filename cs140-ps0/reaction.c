2b7e4fec4ba0f2132fad6a28ca6b9faf06a1906c
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-09-25 15:59:21 +0800

    add reaction

--- cs140-ps0/reaction.c
+++ cs140-ps0/reaction.c
@@ -7,22 +7,59 @@ void make_water();
 
 struct reaction {
 	// FILL ME IN
+	struct lock lock;
+	struct condition cond_o;
+	struct condition cond_h;
+	int num_o;
+	int num_h;
+	int num_w;
 };
 
 void
 reaction_init(struct reaction *reaction)
 {
 	// FILL ME IN
+	lock_init(&reaction->lock);
+	cond_init(&reaction->cond_o);
+	cond_init(&reaction->cond_h);
+	reaction->num_o=0;
+	reaction->num_h=0;
+	reaction->num_w=0;
 }
 
 void
 reaction_h(struct reaction *reaction)
 {
 	// FILL ME IN
+	lock_acquire(&reaction->lock);
+	reaction->num_h++;
+	//fprintf(stderr,"^^%d %d\n",reaction->num_h,reaction->num_o);
+	while(reaction->num_w<=0){
+		cond_signal(&reaction->cond_o,&reaction->lock);
+		cond_wait(&reaction->cond_h,&reaction->lock);
+	}
+	reaction->num_w--;
+	cond_signal(&reaction->cond_o,&reaction->lock);
+	lock_release(&reaction->lock);
 }
 
 void
 reaction_o(struct reaction *reaction)
 {
 	// FILL ME IN
+	lock_acquire(&reaction->lock);
+	fprintf(stderr,"&&%d\n",reaction->num_h);
+	while(reaction->num_h<2){
+		cond_broadcast(&reaction->cond_h,&reaction->lock);
+		cond_wait(&reaction->cond_o,&reaction->lock);
+	}
+	reaction->num_h-=2;
+	reaction->num_w+=2;
+	make_water();
+	while(reaction->num_w>0){
+		cond_broadcast(&reaction->cond_h,&reaction->lock);
+		cond_wait(&reaction->cond_o,&reaction->lock);
+	}
+	cond_broadcast(&reaction->cond_o,&reaction->lock);
+	lock_release(&reaction->lock);
 }
